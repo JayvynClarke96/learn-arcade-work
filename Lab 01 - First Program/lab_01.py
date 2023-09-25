be78977b8a06dc6e28a661ba68f35a96c32e5507
jayvynclarke <Jayvyncool@gmail.com>
2023-09-25 16:15:14 -0400

    Hello world

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-print("Hello World")
+print("Hello Class")
+print("My name is Jayvyn Clarke")
+print("I am taking class CSC 221")
+print("Neil Armstrong said \"That's one small step for man, a giant leap for mankind.\"")
